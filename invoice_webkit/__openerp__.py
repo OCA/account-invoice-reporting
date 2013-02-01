ffb9c7c7f2d3b957c6b3eb458a647653f55af7d3
unknown <nicolas.bessi@camptocamp.com>
2013-02-01 14:49:38 +0100

    [FIX] version number

--- invoice_webkit/__openerp__.py
+++ invoice_webkit/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {   'name': 'Invoice Report using Webkit Library',
-    'version': '1.0',
+    'version': '1.1.0',
     'category': 'Reports/Webkit',
     'description': """
         Replaces the legacy rml Invoice report by a brand new webkit report.
