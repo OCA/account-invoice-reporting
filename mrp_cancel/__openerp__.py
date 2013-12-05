0385e3a190b8a1f11d3a5e4e2f0ff5007a6afe1c
Ray Carnes <rcarnes@ursainfosystems.com>
2013-12-05 15:34:06 -0800

    Changed description

--- mrp_cancel/__openerp__.py
+++ mrp_cancel/__openerp__.py
@@ -16,19 +16,28 @@
     "summary": "Cancel MO's in a single step (Ursa)",
     'description':
         """
-By default, an MO cannot be cancelled unless the associated picking is also cancelled.  This module cancels the picking (if it has not been processed) and the MO in a single step.  All MO related stock moves are cancelled and Stock levels are as you would expect them to be if the MO never existed.  
+One less step when cancelling a Manufacturing Order
+===================================================
+
+        By default, an MO cannot be cancelled unless the associated picking is also cancelled.  This module cancels the picking (if it has not been processed) and the MO in a single step.  All MO related stock moves are cancelled and Stock levels are as you would expect them to be if the MO never existed.  
 
 A detailed description of this module can be found at https://launchpad.net/openerp-shared/7.0/stable/+download/mrp_cancel_README.pdf
 
-OpenERP Version:  7.0
-Ursa Dev Team: RC
+Developer Notes
+---------------
+* OpenERP Version:  7.0
+* Ursa Dev Team: RC
 
-Contact: contact@ursainfosystems.com
+
+Contact
+-------
+* contact@ursainfosystems.com
         """,
     'maintainer': 'Ursa Information Systems',
     'website': 'http://www.ursainfosystems.com',
     "images" : [
         'images/one.png',
+        'images/two.png',
         ],
     "depends" : ["base", "mrp", "stock"],
     "init_xml" : [],
