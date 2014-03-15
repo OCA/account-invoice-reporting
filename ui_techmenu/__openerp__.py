0e99c43cf4dbdca05287dc9bfee3ed6ea11baec1
Ray Carnes <rcarnes@ursainfosystems.com>
2014-03-15 13:49:21 -0700

    Initial module upload

---
+++ ui_techmenu/__openerp__.py
@@ -0,0 +1,49 @@
+﻿# -*- coding: utf-8 -*-
+
+
+
+######################################################################
+#
+#  Note: Program metadata is available in /__init__.py
+#
+######################################################################
+
+{
+    "name" : "Explode Technical Menu",
+    "version" : "1.7",
+    "author" : "Ursa Information Systems",
+    "category" : "User Experience",
+    "summary": "Explode Technical Menu out from under Settings (Ursa)",
+    'description':
+        """
+Overview
+--------
+Moves the Technical Menu out from under Settings so it becomes a top level choice
+
+Developer Notes
+---------------
+* OpenERP Version:  7.0
+* Ursa Dev Team: RC
+
+Contact
+-------
+* contact@ursainfosystems.com
+        """,
+    'maintainer': 'Ursa Information Systems',
+    'website': 'http://www.ursainfosystems.com',
+    "images" : [],
+    "depends" : ["base"],
+    "init_xml" : [],
+
+    "demo_xml" : [],
+    "data" : [
+        'menu.xml',
+    ],
+    "test" : [
+    ],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
