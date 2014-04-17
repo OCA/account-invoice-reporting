fd8fb00e2e80c53c927903076d36019e57d92888
Ray Carnes <rcarnes@ursainfosystems.com>
2014-04-16 20:28:03 -0700

    Initial upload of module

---
+++ l10n_us_administrative/__init__.py
@@ -0,0 +1,32 @@
+﻿# -*- coding: utf-8 -*-
+
+######################################################################
+#
+#  l10n_us_administrative - United States - States & Counties for OpenERP Copyright (C) 2014 - TODAY, Ursa Information Systems (<http://ursainfosystems.com>) 
+#  contact@ursainfosystems.com
+#
+#  Ursa is willing to revoke copyright claims in the future if OpenERP wishes to certify this module.
+#
+######################################################################
+#
+#  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free 
+#  Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+#  For clients with an annual support contract with Ursa, this program is warranted within the guidelines of that contract.
+#
+#  For ALL OTHERS, this program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY (including the absence of implied warranties of 
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE).  See the GNU Affero General Public License for more information.
+#
+######################################################################
+#
+#  You should have received a copy of the GNU Affero General Public License along with this program.  The license is in the file named LICENSE in the top 
+#  level directory and also provided on the web at <http://www.gnu.org/licenses/>.
+#
+######################################################################
+
+# python dependencies (either files or classes) are designated below import <file_dependency> import <class_dependency>
+
+import res_partner
+import res_country
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
