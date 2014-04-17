57d1c9b6b7fe9dfc51365495105b3a86955c4781
Ray Carnes <rcarnes@ursainfosystems.com>
2014-04-16 20:28:03 -0700

    Initial upload of module

---
+++ l10n_us_administrative/res_country.py
@@ -0,0 +1,13 @@
+﻿from openerp.osv import fields, osv
+
+class CountryState(osv.osv):
+
+    _description="State Counties"
+    _name = 'res.country.state.county'
+
+    _columns = {
+        'state_id': fields.many2one('res.country.state', 'State',required=True),
+        'name': fields.char('County', size=33, required=True,
+                            help='United States second level administrative boundaries.'),
+    }
+    _order = 'state_id,name'
---
+++ l10n_us_administrative/res_partner.py
@@ -0,0 +1,10 @@
+﻿from openerp.osv import osv, fields
+
+class res_partner(osv.osv):
+
+    _inherit = "res.partner"
+
+    _columns = {
+        'county_id': fields.many2one("res.country.state.county", 'County'),
+    }
+
