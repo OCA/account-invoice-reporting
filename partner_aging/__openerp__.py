92f05fba9aa039f308efdba5a07f8a08b49c9d29
Guillaume Auger <guillaume.auger@savoirfairelinux.com>
2015-02-10 10:21:50 -0500

    [ADD]partner-aging with currency column

--- partner_aging/__openerp__.py
+++ partner_aging/__openerp__.py
@@ -1,43 +1,42 @@
 # -*- coding: utf-8 -*-
-######################################################################
+###############################################################################
 #
-#  Note: Program metadata is available in /__init__.py
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 Ursa Informative Systems (<www.ursainfosystems.com>).
 #
-######################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
 {
-    "name" : "Interactive Partner Aging",
-    "version" : "1.7.1",
-    "author" : 'Ursa Information Systems',
+    "name": "Interactive Partner Aging",
+    "version": "1.7.2",
+    "author": 'Ursa Information Systems',
     "summary": "Aging as a view - invoices and credits (Ursa)",
-    "description" : """
-This module creates new AR and AP views.
-
-The default OpenERP Aged Partner balance report is a static PDF that is based on the difference between credits and debits, not based on documents such as Invoices and Payments.  It also does not consider unapplied payments.  This version is interactive and more complete.
-
-A detailed description of this module can be found at https://launchpad.net/openerp-shared/7.0/stable/+download/partner_aging_README.pdf
-
-OpenERP Version:  7.0
-Ursa Dev Team: RC
-
-Contact: contact@ursainfosystems.com
-""",
     'maintainer': 'Ursa Information Systems',
     'website': 'http://www.ursainfosystems.com',
     "category": 'Accounting & Finance',
-    "images" : [],
-    "depends" : ["base","account_accountant"],
-    "data" : [
-              'partner_aging_supplier.xml',
-              'partner_aging_customer.xml',
-              'security/ir.model.access.csv',
+    "images": [],
+    "depends": ["base", "account_accountant"],
+    "data": [
+        'partner_aging_supplier.xml',
+        'partner_aging_customer.xml',
+        'security/ir.model.access.csv',
     ],
-    "test" : [
+    "test": [
     ],
     "auto_install": False,
     "application": False,
     "installable": True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
