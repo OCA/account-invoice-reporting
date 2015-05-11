5786d73973fb2f1f5bb22d2f48a1decfc330fbf9
Charbel Jacquin <charbel.jacquin@camptocamp.com>
2015-05-11 18:03:57 +0200

    revise imports

--- invoice_webkit/invoice.py
+++ invoice_webkit/invoice.py
@@ -17,7 +17,8 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##############################################################################
-from openerp.osv.orm import Model, fields
+from openerp.osv import fields
+from openerp.osv.orm import Model
 from openerp.osv import osv
 from openerp.tools.translate import _
 
