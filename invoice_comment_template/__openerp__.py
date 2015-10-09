581707efc7997ccd66ff3d4f421ed7b2c30562c7
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:28 +0200

    [UPD] prefix versions with 8.0

--- invoice_comment_template/__openerp__.py
+++ invoice_comment_template/__openerp__.py
@@ -21,7 +21,7 @@
 
 {'name': 'Invoice Comments',
  'summary': 'Comments templates on invoice documents',
- 'version': '1.0',
+ 'version': '8.0.1.0.0',
  'depends': ['account',
              'base_comment_template',
              ],
