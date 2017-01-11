0d1407b23377c46b6612d55c700b0fac9fb74dfe
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-01-12 00:43:37 +0100

    [ADD] account_reporting_weight
    =====================================
    Weights in the invoices analysis view
    =====================================
    
    This module adds the measure "Weight" in the invoices analysis view. This is
    caught from 2 possible sources:
    
    * If the UoM of the product is one of the category "Weight", the value is taken
      from the ordered quantity.
    * If the UoM of the product is another, then the weight is taken from the
      weight field of the product multiply by the ordered quantity.
    
    Configuration
    =============
    
    You need to be at least "Accountant" on "Accounting & Finance" role for
    seeing the report.
    
    Usage
    =====
    
    * Go to *Invoicing > Reporting > Businness Intelligence > Invoices*.
    * Add the "Weight" measure from your "Measures" dropdown in your analysis.
    
    Known issues / Roadmap
    ======================
    
    * The weight quantity is expressed in the unit of measure of the product,
      so if you have several weight UoMs across your products, the global sum won't
      make sense.

---
+++ account_reporting_weight/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Weights in the invoices analysis view',
+    'version': '9.0.1.0.0',
+    'author': 'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'category': 'Inventory, Logistics, Warehousing',
+    'license': 'AGPL-3',
+    'website': 'https://www.tecnativa.com',
+    'depends': [
+        'sale',
+    ],
+    'installable': True,
+}
