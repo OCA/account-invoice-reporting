877f1420c53ca17b67eaa67cc50d20465080feb5
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-03-16 14:56:10 +0100

    [ADD] account_invoice_report_due_list: New module

---
+++ account_invoice_report_due_list/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Account Invoice Report Due List',
+    'summary': 'Show multiple due data in invoice',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting',
+    'website': 'https://github.com/OCA/account-invoice-reporting',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice_view.xml',
+        'views/report_invoice.xml',
+    ],
+}
