1460eb7d46d4a6f168985a98fd232511fafd8d3f
Mehdi Ghezal <mehdi.ghezal@gmail.com>
2018-04-03 20:02:48 +0700

    MIG tests

--- account_invoice_production_lot/tests/test_invoice_production_lot.py
+++ account_invoice_production_lot/tests/test_invoice_production_lot.py
@@ -19,154 +19,79 @@
 #
 ##############################################################################
 from openerp.tests import common
-from datetime import date
 
 
-class TestProdLot(common.TransactionCase):
+class TestProdLot(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestProdLot, cls).setUpClass()
 
-    def setUp(self):
-        super(TestProdLot, self).setUp()
-        self.sale_order = self.env["sale.order"]
-        self.stock_move = self.env["stock.move"]
-        self.stock_picking = self.env["stock.picking"]
-        self.account_invoice = self.env["account.invoice"]
-        self.stock_transfer_details = self.registry("stock.transfer_details")
-        self.stock_invoice = self.registry("stock.invoice.onshipping")
-        self.order_invoice = self.registry("sale.advance.payment.inv")
-        self.model_data = self.env['ir.model.data']
+        cls.product = cls.env['product.product'].create({
+            'name': 'Product Test',
+            'type': 'product'
+        })
 
-    def getDemoObject(self, module, data_id):
-        if module == '':
-            module = 'account_invoice_production_lot'
-        xmlid = '%s.%s' % (module, data_id)
-        return self.model_data.xmlid_to_object(xmlid)
+        cls.lot1 = cls.env['stock.production.lot'].create({
+            'name': 'Lot 1',
+            'product_id': cls.product.id
+        })
 
-    def getIdDemoObj(self, module, data_id):
-        return self.getDemoObject(module, data_id).id
+        cls.lot2 = cls.env['stock.production.lot'].create({
+            'name': 'Lot 2',
+            'product_id': cls.product.id
+        })
 
-    def run_picking(self, pick, lot_ids, split=False):
-        cr = self.env.cr
-        uid = self.env.uid
-        context = self.env.context.copy()
-        context.update(
-            {
-                'active_model': 'stock.picking',
-                'active_ids': [pick.id],
-                'active_id': len([pick.id]) and pick.id or False
-            }
-        )
-        pick_wizard_id = self.stock_transfer_details.create(
-            cr, uid, {'picking_id': pick.id}, context)
-        pick_wizard = self.stock_transfer_details.browse(
-            cr, uid, pick_wizard_id)
-        if split:
-            pick_wizard.item_ids[0].split_quantities()
-            pick_wizard.refresh()
-        for count in range(len(lot_ids)):
-            pick_wizard.item_ids[count].write({'lot_id': lot_ids[count].id})
-        return self.stock_transfer_details.do_detailed_transfer(
-            cr, uid, [pick_wizard_id], context)
+        cls.partner = cls.env['res.partner'].create({
+            'name': 'Test partner'
+        })
 
-    def run_create_invoice(self, pick):
-        cr = self.env.cr
-        uid = self.env.uid
-        context = self.env.context.copy()
-        context.update({'active_ids': [pick.id]})
-        inv_wizard_id = self.stock_invoice.create(
-            cr, uid,
-            {
-                'invoice_date': date.today().strftime('%Y-%m-%d')
-            }, context)
-        return self.stock_invoice.create_invoice(
-            cr, uid, [inv_wizard_id], context)
+        cls.sale = cls.env['sale.order'].create({
+            'partner_id': cls.partner.id,
+            'partner_invoice_id': cls.partner.id,
+            'partner_shipping_id': cls.partner.id,
+            'order_line': [
+                (0, 0, {
+                    'product_id': cls.product.id,
+                    'name': cls.product.name,
+                    'product_uom_qty': 2,
+                    'product_uom': cls.product.uom_id.id,
+                    'price_unit': 15.0
+                })
+            ]
+        })
 
-    def test_0_SaleOrder(self):
-        """
-        Test Sale Order 1
-       """
-        lot_ids = []
-        lot_ids.append(self.getDemoObject('', 'lot_icecream_0'))
-        order = self.getDemoObject('', 'sale_order_0')
-        order.signal_workflow('order_confirm')
-        for pick in order.picking_ids:
-            data = pick.force_assign()
-            self.assertEqual(pick.state, 'assigned')
-            self.assertTrue(data)
-            self.run_picking(pick, lot_ids)
-            done = pick.action_done()
-            self.assertTrue(done)
-            self.assertEqual(pick.state, 'done')
-            invoice_id = self.run_create_invoice(pick)
-            invoice = self.account_invoice.browse(invoice_id)
-            self.assertEqual(
-                invoice.invoice_line[0].prod_lot_ids[0].name,
-                'Lot0 for Ice cream'
-            )
-            self.assertEqual(
-                invoice.invoice_line[0].lot_formatted_note,
-                '<ul><li>S/N Lot0 for Ice cream</li></ul>'
-            )
+        cls.stock_location = cls.env.ref('stock.stock_location_stock')
+        cls.customer_location = cls.env.ref('stock.stock_location_customers')
+        cls.picking_type_out = cls.env.ref('stock.picking_type_out')
 
-    def test_1_SaleOrder(self):
-        """
-        Test Sale Order 1 split quantity in picking
-        and set a  serial number Lot, for each item
-       """
-        lot_ids = []
-        lot_ids.append(self.getDemoObject('', 'lot_icecream_0'))
-        lot_ids.append(self.getDemoObject('', 'lot_icecream_1'))
-        order = self.getDemoObject('', 'sale_order_1')
-        order.signal_workflow('order_confirm')
-        self.assertEqual(len(order.picking_ids), 1)
-        for pick in order.picking_ids:
-            data = pick.force_assign()
-            self.assertEqual(pick.state, 'assigned')
-            self.assertTrue(data)
-            self.run_picking(pick, lot_ids, split=True)
-            done = pick.action_done()
-            self.assertTrue(done)
-            self.assertEqual(pick.state, 'done')
-            invoice_id = self.run_create_invoice(pick)
-            invoice = self.account_invoice.browse(invoice_id)
-            self.assertEqual(
-                invoice.invoice_line[0].prod_lot_ids[0].name,
-                'Lot0 for Ice cream'
-            )
-            self.assertEqual(
-                invoice.invoice_line[0].prod_lot_ids[1].name,
-                'Lot1 for Ice cream'
-            )
-            self.assertEqual(
-                invoice.invoice_line[0].lot_formatted_note,
-                '<ul><li>S/N Lot0 for Ice cream</li> '
-                '<li>S/N Lot1 for Ice cream</li></ul>'
-            )
+    def test_invoice_line_lots(self):
+        self.sale.action_confirm()
+        picking = self.sale.picking_ids[:1]
 
-    def test_2_SaleOrder(self):
-        """
-        Test Sale Order 2
-       """
-        lot_ids = []
-        lot_ids.append(self.getDemoObject('', 'lot_icecream_0'))
-        order = self.getDemoObject('', 'sale_order_2')
-        order.signal_workflow('order_confirm')
-        self.assertEqual(len(order.picking_ids), 1)
-        for pick in order.picking_ids:
-            data = pick.force_assign()
-            self.assertEqual(pick.state, 'assigned')
-            self.assertTrue(data)
-            trans = self.run_picking(pick, lot_ids)
-            done = pick.action_done()
-            self.assertTrue(done)
-            self.assertTrue(trans)
-            self.assertEqual(pick.state, 'done')
-            res = order.manual_invoice()
-            invoice = self.account_invoice.browse(res['res_id'])
-            self.assertEqual(
-                invoice.invoice_line[0].prod_lot_ids[0].name,
-                'Lot0 for Ice cream'
-            )
-            self.assertEqual(
-                invoice.invoice_line[0].lot_formatted_note,
-                '<ul><li>S/N Lot0 for Ice cream</li></ul>'
-            )
+        picking.action_confirm()
+        picking.action_assign()
+
+        picking.do_prepare_partial()
+
+        self.env['stock.pack.operation'].create({
+            'product_id': self.product.id,
+            'product_qty': 2,
+            'product_uom_id': self.product.uom_id.id,
+            'location_id': self.stock_location.id,
+            'location_dest_id': self.customer_location.id,
+            'picking_id': picking.id,
+            'pack_lot_ids': [
+                (0, 0, {'lot_id': self.lot1.id, 'qty': 1}),
+                (0, 0, {'lot_id': self.lot2.id, 'qty': 1})
+            ]
+        })
+
+        picking.do_transfer()
+
+        invoice_id = self.sale.action_invoice_create()
+        invoice = self.env['account.invoice'].browse(invoice_id)
+
+        self.assertEqual(len(invoice.invoice_line_ids), 1)
+        self.assertEqual(len(invoice.invoice_line_ids.prod_lot_ids.ids), 2)
+        self.assertIn('Lot 1', invoice.invoice_line_ids.lot_formatted_note)
+        self.assertIn('Lot 2', invoice.invoice_line_ids.lot_formatted_note)
