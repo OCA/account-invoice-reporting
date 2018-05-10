8b86a961c9d1f95be4c6e6d3d082c277890819b4
cubells <vicent@vcubells.net>
2018-05-10 16:21:35 +0200

    [IMP] invoice_comment_template: add template to partner (#71)

--- invoice_comment_template/models/account_invoice.py
+++ invoice_comment_template/models/account_invoice.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014 Guewen Baconnier (Camptocamp SA)
 # Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
+# Copyright 2018 Vicent Cubells - Tecnativa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -27,3 +28,13 @@ class AccountInvoice(models.Model):
         comment = self.comment_template2_id
         if comment:
             self.note2 = comment.get_value(self.partner_id.id)
+
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        res = super(AccountInvoice, self)._onchange_partner_id()
+        comment_template = self.partner_id.comment_template_id
+        if comment_template.position == 'before_lines':
+            self.comment_template1_id = comment_template
+        elif comment_template.position == 'after_lines':
+            self.comment_template2_id = comment_template
+        return res
