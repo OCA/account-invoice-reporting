OCA/account-invoicing

[9.0][WIP] account_invoice_group_picking: WIP

account_invoice_group_picking/__openerp__.py
@@ -1,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Group Picking",
+    "summary": "Print invoice lines grouped by picking",
+    "version": "9.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+}

OCA/account-invoice-reporting

account_invoice_group_picking/__openerp__.py
@@ -1,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Group Picking",
+    "summary": "Print invoice lines grouped by picking",
+    "version": "9.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/portal_security.xml",
+        "views/report_invoice.xml",
+    ],
+}