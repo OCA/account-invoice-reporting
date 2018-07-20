4732dae87ee77197e0e416602ccfde46f35f29f6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-07-20 13:16:37 -0500

    PEP8 [IMP] README

--- partner_time_to_pay/__manifest__.py
+++ partner_time_to_pay/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Open Source Integrators <https://www.opensourceintegrators.com>
+# Copyright 2017 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -6,9 +6,11 @@
     'summary': 'Add receivables and payables statistics to partners',
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
+    'development_status': "Beta",
     'author': 'Open Source Integrators, Odoo Community Association (OCA)',
     'category': 'Accounting & Finance',
-    'website': 'http://www.opensourceintegrators.com',
+    'website': 'https://www.opensourceintegrators.com',
+    "maintainers": ['max3903'],
     'depends': ['account'],
     'data': [
         'views/res_partner.xml',
