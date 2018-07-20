8fb17c2680b35f0bf9bbf65558f85df23561305e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-07-20 13:16:37 -0500

    PEP8 [IMP] README

--- partner_time_to_pay/__init__.py
+++ partner_time_to_pay/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Open Source Integrators <https://www.opensourceintegrators.com>
+# Copyright 2017 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
