d3c679d5ab38f376256a537b836f9f0b19f29921
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-08-22 12:00:11 +0200

    [FIX] account_invoice_comment_template: Finish migration
    * Simplify and fix tests
    * Rename translations

--- account_invoice_comment_template/__manifest__.py
+++ account_invoice_comment_template/__manifest__.py
@@ -1,13 +1,19 @@
 # Copyright 2014 Guewen Baconnier (Camptocamp SA)
 # Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
+# Copyright 2018 Qubiq - Xavier Jiménez
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {'name': 'Account Invoice Comments',
  'summary': 'Comments templates on invoice documents',
  'version': '11.0.1.0.0',
- 'depends': ['account',
-             'base_comment_template',
-             ],
- 'author': "Camptocamp, Odoo Community Association (OCA)",
+ 'depends': [
+     'account',
+     'base_comment_template',
+ ],
+ 'author': "Camptocamp, "
+           "Tecnativa, "
+           "Odoo Community Association (OCA)",
  "license": "AGPL-3",
  'data': ['views/account_invoice_view.xml',
           'views/base_comment_template_view.xml',
