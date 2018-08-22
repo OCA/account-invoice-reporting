00486f0812c81698d6b04a890114c7aeb1fc65b3
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-08-22 12:00:11 +0200

    [FIX] account_invoice_comment_template: Finish migration
    * Simplify and fix tests
    * Rename translations

--- account_invoice_comment_template/tests/test_account_invoice_report.py
+++ account_invoice_comment_template/tests/test_account_invoice_report.py
@@ -1,25 +1,29 @@
 # Copyright 2017 Simone Rubino - Agile Business Group
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
-from odoo import report
 
 
 class TestAccountInvoiceReport(TransactionCase):
-    def setUp(self, *args, **kwargs):
+    at_install = False
+    post_install = True
+
+    def setUp(self):
         super(TestAccountInvoiceReport, self).setUp()
         self.base_comment_model = self.env['base.comment.template']
         self.before_comment = self._create_comment('before_lines')
         self.after_comment = self._create_comment('after_lines')
-        self.partner_id = self.env['res.partner'].create({
+        self.partner = self.env['res.partner'].create({
             'name': 'Partner Test'
         })
         self.invoice_model = self.env['account.invoice']
         self.invoice = self.invoice_model.create({
-            'partner_id': self.env.ref('base.res_partner_1').id,
+            'partner_id': self.partner.id,
             'comment_template1_id': self.before_comment.id,
             'comment_template2_id': self.after_comment.id
         })
+
         self.invoice._set_note1()
         self.invoice._set_note2()
 
@@ -31,25 +35,19 @@ class TestAccountInvoiceReport(TransactionCase):
         })
 
     def test_comments_in_invoice(self):
-        (res, _) = report. \
-            render_report(self.env.cr, self.env.uid,
-                          [self.invoice.id], 'account.report_invoice', {})
-        self.assertRegexpMatches(res, self.before_comment.text)
-        self.assertRegexpMatches(res, self.after_comment.text)
+        res = self.env['ir.actions.report']._get_report_from_name(
+            'account.report_invoice'
+        ).render_qweb_html(self.invoice.ids)
+        self.assertRegexpMatches(str(res[0]), self.before_comment.text)
+        self.assertRegexpMatches(str(res[0]), self.after_comment.text)
 
     def test_onchange_partner_id(self):
-        self.partner_id.comment_template_id = self.after_comment.id
-        vals = {
-            'partner_id': self.partner_id.id,
-        }
-        new_invoice = self.env['account.invoice'].new(vals)
+        self.partner.comment_template_id = self.after_comment.id
+        new_invoice = self.env['account.invoice'].new({
+            'partner_id': self.partner.id,
+        })
         new_invoice._onchange_partner_id()
-        invoice_dict = new_invoice._convert_to_write(new_invoice._cache)
-        new_invoice = self.env['account.invoice'].create(invoice_dict)
         self.assertEqual(new_invoice.comment_template2_id, self.after_comment)
-        self.partner_id.comment_template_id = self.before_comment.id
-        new_invoice = self.env['account.invoice'].new(vals)
+        self.partner.comment_template_id = self.before_comment.id
         new_invoice._onchange_partner_id()
-        invoice_dict = new_invoice._convert_to_write(new_invoice._cache)
-        new_invoice = self.env['account.invoice'].create(invoice_dict)
         self.assertEqual(new_invoice.comment_template1_id, self.before_comment)
