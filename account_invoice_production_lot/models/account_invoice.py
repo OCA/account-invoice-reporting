953efd7162e8f7505dbc1ced395609d514fe3042
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-12-15 02:42:06 +0100

    [MIG] account_invoice_production_lot: Migration to 11.0
    * Standard procedure
    * Adapt code to new stock data model
    * Adapt tests
    * Fix README layout
    * Remove coding lines in .py

--- account_invoice_production_lot/models/account_invoice.py
+++ account_invoice_production_lot/models/account_invoice.py
@@ -1,24 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2011 Domsense s.r.l. <http://www.domsense.com>
 # Copyright 2013 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 # Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
-from odoo.tools.misc import formatLang
+from odoo import _, api, fields, models
 
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
-
-    order_line_ids = fields.Many2many(
-        comodel_name='sale.order.line',
-        relation='sale_order_line_invoice_rel',
-        column1='invoice_line_id',
-        column2='order_line_id',
-        string='Order Lines',
-        readonly=True,
-    )
 
     prod_lot_ids = fields.Many2many(
         comodel_name='stock.production.lot',
@@ -34,42 +24,26 @@ class AccountInvoiceLine(models.Model):
     @api.multi
     def _compute_prod_lots(self):
         for line in self:
-            line.prod_lot_ids = line.move_line_ids.mapped('lot_ids')
+            line.prod_lot_ids = line.mapped(
+                'move_line_ids.move_line_ids.lot_id'
+            )
 
     @api.multi
     def _compute_line_lots(self):
         for line in self:
-            if line.prod_lot_ids:
-                res = line.quantity_by_lot()
-                note = u'<ul>'
-                lot_strings = []
-                for lot in line.prod_lot_ids:
-                    lot_string = u'<li>S/N %s%s</li>' % (
-                        lot.name, u' (%s)' % res[lot] if res.get(lot) else '')
-                    lot_strings.append(lot_string)
-                note += u' '.join(lot_strings)
-                note += u'</ul>'
-                line.lot_formatted_note = note
-
-    def quantity_by_lot(self):
-        self.ensure_one()
-        move_ids = self.move_line_ids
-        res = {}
-        for move in move_ids:
-            for quant in move.quant_ids:
-                if (
-                    quant.lot_id and
-                    quant.location_id.id == move.location_dest_id.id
-                ):
-                    if quant.lot_id not in res:
-                        res[quant.lot_id] = quant.qty
+            note = '<ul>'
+            lot_strings = []
+            for sml in line.mapped('move_line_ids.move_line_ids'):
+                if sml.lot_id:
+                    if sml.product_id.tracking == 'serial':
+                        lot_strings.append('<li>%s %s</li>' % (
+                            _('S/N'), sml.lot_id.name,
+                        ))
                     else:
-                        res[quant.lot_id] += quant.qty
-        for lot in res:
-            if lot.product_id.tracking == 'lot':
-                res[lot] = formatLang(self.env, res[lot])
-            else:
-                # If not tracking By lots or not By Unique Serial Number,
-                # quantity is not relevant
-                res[lot] = False
-        return res
+                        lot_strings.append('<li>%s %s (%s)</li>' % (
+                            _('Lot'), sml.lot_id.name, sml.qty_done,
+                        ))
+            if lot_strings:
+                note += ' '.join(lot_strings)
+                note += '</ul>'
+                line.lot_formatted_note = note
