85a34aa99a957ee3d966c5c77035b25d676c8a30
kong <rattapong.chok@gmail.com>
2019-07-11 16:09:59 +0700

    [12.0][MIG]account_invoice_reporting - WIP

--- account_reporting_weight/__manifest__.py
+++ account_reporting_weight/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Weights in the invoices analysis view',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
     'category': 'Inventory, Logistics, Warehousing',
