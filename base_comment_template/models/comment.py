8a011cb804b765c644c7cd40ad66c2765a189c4f
emagdalena <emagdalena@c2i.es>
2019-10-17 00:08:47 +0200

    [IMP] base_comment_template: black, isort

--- base_comment_template/models/comment.py
+++ base_comment_template/models/comment.py
@@ -2,36 +2,26 @@
 # Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class BaseCommentTemplate(models.Model):
     _name = "base.comment.template"
     _description = "Base comment template"
 
-    name = fields.Char(
-        string='Comment summary',
-        required=True
-    )
+    name = fields.Char(string="Comment summary", required=True)
     position = fields.Selection(
-        selection=[
-            ('before_lines', 'Before lines'),
-            ('after_lines', 'After lines')
-        ],
+        selection=[("before_lines", "Before lines"), ("after_lines", "After lines")],
         required=True,
-        default='before_lines',
-        help="Position on document"
+        default="before_lines",
+        help="Position on document",
     )
-    text = fields.Html(
-        string='Comment',
-        translate=True,
-        required=True
-    )
+    text = fields.Html(string="Comment", translate=True, required=True)
 
     @api.multi
     def get_value(self, partner_id=False):
         self.ensure_one()
         lang = None
         if partner_id:
-            lang = self.env['res.partner'].browse(partner_id).lang
-        return self.with_context({'lang': lang}).text
+            lang = self.env["res.partner"].browse(partner_id).lang
+        return self.with_context({"lang": lang}).text
