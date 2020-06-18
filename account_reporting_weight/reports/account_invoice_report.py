25f85763b0dae1add1b8ace690f5f77480e5de87
Carlos Roca <carlos.roca@tecnativa.com>
2020-06-18 12:49:38 +0200

    [MIG] account_reporting_weight: migration to v13.0
    [UPD] Update account_reporting_weight.pot
    
    [UPD] README.rst

--- account_reporting_weight/reports/account_invoice_report.py
+++ account_reporting_weight/reports/account_invoice_report.py
@@ -3,30 +3,17 @@
 
 from odoo import fields, models
 
-import odoo.addons.decimal_precision as dp
-
 
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
-    weight = fields.Float(digits=dp.get_precision("Stock Weight"))
+    weight = fields.Float(digits="Stock Weight")
 
     def _select(self):
         select_str = super()._select()
         select_str += """
-            , sub.weight as weight
+            , SUM(product.weight * line.quantity
+            / uom_line.factor * uom_template.factor)
+            as weight
             """
         return select_str
-
-    def _sub_select(self):
-        select_str = super()._sub_select()
-        select_str += """
-            , SUM(pr.weight * ail.quantity / u.factor * u2.factor)
-            AS weight
-            """
-        return select_str
-
-    def _group_by(self):
-        group_by_str = super()._group_by()
-        group_by_str += ", pr.weight, u.category_id"
-        return group_by_str
