8366fa470bd7f695d0af984c08b385e1d6319bbf
Carlos Roca <carlos.roca@tecnativa.com>
2020-06-18 11:48:02 +0200

    [IMP] account_reporting_weight: black, isort, prettier

--- account_reporting_weight/reports/account_invoice_report.py
+++ account_reporting_weight/reports/account_invoice_report.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+
 import odoo.addons.decimal_precision as dp
 
 
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
-    weight = fields.Float(digits=dp.get_precision('Stock Weight'))
+    weight = fields.Float(digits=dp.get_precision("Stock Weight"))
 
     def _select(self):
         select_str = super()._select()
