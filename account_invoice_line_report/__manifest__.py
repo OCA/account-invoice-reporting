1422c4c7aad445b64cbb492b10c9e8f490da7b27
João Marques <joao.marques@tecnativa.com>
2020-10-05 07:13:44 +0100

    [IMP] account_invoice_line_report: black, isort, prettier

--- account_invoice_line_report/__manifest__.py
+++ account_invoice_line_report/__manifest__.py
@@ -3,19 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Invoice Line Report',
-    'summary': 'New view to manage invoice lines information',
-    'version': '12.0.1.0.0',
-    'category': 'Account',
-    'website': 'https://github.com/OCA/account-invoice-reporting',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
-    ],
-    'data': [
-        'report/account_invoice_report_view.xml',
-    ],
-    'installable': True,
+    "name": "Account Invoice Line Report",
+    "summary": "New view to manage invoice lines information",
+    "version": "12.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/OCA/account-invoice-reporting",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["account",],
+    "data": ["report/account_invoice_report_view.xml",],
+    "installable": True,
 }
