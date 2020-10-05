77674e15916ea15df0cba498837e4f2fd032f778
João Marques <joao.marques@tecnativa.com>
2020-10-05 07:19:56 +0100

    [MIG] account_invoice_line_report: Migration to 13.0
    [UPD] Update account_invoice_line_report.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-invoice-reporting-13.0/account-invoice-reporting-13.0-account_invoice_line_report
    Translate-URL: https://translation.odoo-community.org/projects/account-invoice-reporting-13-0/account-invoice-reporting-13-0-account_invoice_line_report/

--- account_invoice_line_report/__manifest__.py
+++ account_invoice_line_report/__manifest__.py
@@ -1,16 +1,17 @@
-# Copyright 2017 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
-# Copyright 2018 Vicent Cubells - Tecnativa <vicent.cubells@tecnativa.com>
+# Copyright 2017 Tecnativa - Carlos Dauden
+# Copyright 2018 Tecnativa - Vicent Cubells
+# Copyright 2020 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Invoice Line Report",
     "summary": "New view to manage invoice lines information",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/account-invoice-reporting",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": ["account",],
-    "data": ["report/account_invoice_report_view.xml",],
+    "depends": ["account"],
+    "data": ["report/account_invoice_report_view.xml"],
     "installable": True,
 }
