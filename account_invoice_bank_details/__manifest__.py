46afed4d1691345ef677fdad6b63f44bc58af94d
vrenaville <vincent.renaville@camptocamp.com>
2021-01-08 09:37:25 +0100

    [V13] Add account invoice bank details

---
+++ account_invoice_bank_details/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Invoice bank account details",
+    "summary": "Select bank account base on currency + print bank details on report"
+    "reports and customer portal",
+    "version": "13.0.1.0.0",
+    "category": "Sales Management",
+    "website": "http://github.com/OCA/account-invoice-repoting",
+    "author": "Camptocamp, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/invoice_report_templates.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
