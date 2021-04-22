cb92331b77c74b5bb6c5e954067ecffe6d3f69d0
david <david.vidal@tecnativa.com>
2021-04-22 14:30:21 +0200

    [IMP] account_invoice_report_grouped_by_picking_sale_mrp: black, isort, prettier

--- account_invoice_report_grouped_by_picking_sale_mrp/models/account_invoice.py
+++ account_invoice_report_grouped_by_picking_sale_mrp/models/account_invoice.py
@@ -5,13 +5,14 @@ from odoo import models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     def _get_signed_quantity_done(self, invoice_line, move, sign):
         res = super()._get_signed_quantity_done(invoice_line, move, sign)
-        bom = self.env['mrp.bom']._bom_find(
-            product=invoice_line.product_id, company_id=self.company_id.id)
-        if bom and bom.type == 'phantom':
+        bom = self.env["mrp.bom"]._bom_find(
+            product=invoice_line.product_id, company_id=self.company_id.id
+        )
+        if bom and bom.type == "phantom":
             bom_line_data = bom.explode(invoice_line.product_id, 1)[1]
-            res /= sum(map(lambda r: r[1]['qty'], bom_line_data))
+            res /= sum(map(lambda r: r[1]["qty"], bom_line_data))
         return res
