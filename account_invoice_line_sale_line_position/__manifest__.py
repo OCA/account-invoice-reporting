28a62501b79daf8a1a9de2b44525ccd870742999
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2022-05-17 14:44:26 +0200

    [MIG] account_invoice_line_sale_line_position: Migration to 15.0

--- account_invoice_line_sale_line_position/__manifest__.py
+++ account_invoice_line_sale_line_position/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Invoice Line Sale Line Position",
     "summary": "Adds the related sale line position on invoice line.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Sale",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
