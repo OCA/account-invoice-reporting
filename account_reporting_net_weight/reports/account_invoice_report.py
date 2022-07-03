d2e882f046a085af07e58def2e340897e104e10e
cubells <vicent@vcubells.net>
2022-07-03 13:37:26 +0200

    [MIG] account_reporting_net_weight: Migration to 13.0

--- account_reporting_net_weight/reports/account_invoice_report.py
+++ account_reporting_net_weight/reports/account_invoice_report.py
@@ -1,28 +1,22 @@
-# Copyright 2022 Trey, Kilobytes de Soluciones - Vicent Cubells
+# Copyright 2023 Trey, Kilobytes de Soluciones - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import fields, models
-import odoo.addons.decimal_precision as dp
 
 
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
-    net_weight = fields.Float(
-        digits=dp.get_precision("Stock Weight"),
-    )
+    net_weight = fields.Float(digits="Stock Weight")
 
     def _select(self):
         select_str = super()._select()
         select_str += """
-            , sub.net_weight as net_weight
-            """
-        return select_str
-
-    def _sub_select(self):
-        select_str = super()._sub_select()
-        select_str += """
-            , SUM (
-                (invoice_type.sign_qty * ail.quantity * pr.net_weight) /
-                COALESCE(u.factor,1) * COALESCE(u2.factor,1)) AS net_weight
+            , sum(product.net_weight) / NULLIF(COALESCE(uom_line.factor, 1) /
+                  COALESCE(uom_template.factor, 1), 0.0) * (
+                      CASE WHEN move.type IN (
+                          'in_invoice','out_refund','in_receipt')
+                      THEN -1 ELSE 1 END)
+               AS net_weight
             """
         return select_str
