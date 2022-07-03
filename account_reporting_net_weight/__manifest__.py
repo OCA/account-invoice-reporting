56bef9fba0b42cc24ab7ad4c04539a6e5fde599e
cubells <vicent@vcubells.net>
2022-07-03 13:37:26 +0200

    [MIG] account_reporting_net_weight: Migration to 13.0

--- account_reporting_net_weight/__manifest__.py
+++ account_reporting_net_weight/__manifest__.py
@@ -1,18 +1,14 @@
-# Copyright 2022 Trey, Kilobytes de Soluciones - Vicent Cubells
+# Copyright 2023 Trey, Kilobytes de Soluciones - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Net Weights in the invoices analysis view",
-    "version": "12.0.1.0.0",
-    "author": "Trey,"
-              "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Trey," "Odoo Community Association (OCA)",
     "category": "Inventory, Logistics, Warehousing",
-    "development_status": "Production/Stable",
+    "development_status": "Beta",
     "maintainers": ["cubells"],
     "license": "AGPL-3",
-    "website": "https://www.github.com/oca/account-invoice-reporting/",
-    "depends": [
-        "account",
-        "product_net_weight",
-    ],
+    "website": "https://github.com/OCA/account-invoice-reporting",
+    "depends": ["sale", "product_net_weight"],
     "installable": True,
 }
