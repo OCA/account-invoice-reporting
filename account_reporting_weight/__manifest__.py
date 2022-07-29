f78be715eed83aac8091099cdf0c0efa2fc907e7
Víctor Martínez <victor.martinez@tecnativa.com>
2022-07-29 09:54:41 +0200

    [MIG] account_reporting_weight: Migration to 15.0 TT36435

--- account_reporting_weight/__manifest__.py
+++ account_reporting_weight/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Weights in the invoices analysis view",
-    "version": "13.0.1.0.1",
+    "version": "15.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "category": "Inventory, Logistics, Warehousing",
     "development_status": "Production/Stable",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-invoice-reporting",
-    "depends": ["sale"],
+    "depends": ["account"],
     "installable": True,
 }
--- account_reporting_weight/readme/USAGE.rst
+++ account_reporting_weight/readme/USAGE.rst
@@ -1,2 +1,2 @@
-#. Go to *Invoicing > Reporting > Management > Invoices*.
+#. Go to *Invoicing > Reporting > Management > Invoice Analysis*.
 #. Add the "Weight" measure from your "Measures" dropdown in your analysis.
