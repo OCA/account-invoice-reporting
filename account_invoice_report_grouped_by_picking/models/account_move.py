b8e7b41fad39932e55e92f2d0a632ab0f9e1169e
David <david.vidal@tecnativa.com>
2022-11-08 17:53:14 +0100

    [FIX] account_invoice_report_grouped_by_picking: refund invoice report
    Consider pending pickings that are linked to the invoice.
    
    TT40412

--- account_invoice_report_grouped_by_picking/models/account_move.py
+++ account_invoice_report_grouped_by_picking/models/account_move.py
@@ -5,7 +5,7 @@
 
 from collections import OrderedDict
 
-from odoo import api, fields, models
+from odoo import api, models
 from odoo.tools import float_is_zero
 
 
@@ -17,8 +17,7 @@ class AccountMove(models.Model):
         return sorted(
             lines_dic,
             key=lambda x: (
-                x["picking"].date or fields.Datetime.now(),
-                x["picking"].date_done or fields.Datetime.now(),
+                (x["picking"].date, x["picking"].date_done or x["picking"].date)
             ),
         )
 
