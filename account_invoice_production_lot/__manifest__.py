4884d8d976dc6e05ba4bfb8e6dc3098451a1a834
Víctor Martínez <victor.martinez@tecnativa.com>
2023-01-18 17:09:59 +0100

    [MIG] account_invoice_production_lot: Migration to 15.0
    TT37267

--- account_invoice_production_lot/__manifest__.py
+++ account_invoice_production_lot/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Invoice Production Lots",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "author": "Agile Business Group," "Tecnativa," "Odoo Community Association (OCA)",
     "summary": "Display delivered serial numbers in invoice",
     "website": "https://github.com/OCA/account-invoice-reporting",
