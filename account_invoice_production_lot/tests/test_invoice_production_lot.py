fdd8b3b7784bda627216ce40394a077af0e6f0f4
Víctor Martínez <victor.martinez@tecnativa.com>
2023-01-18 17:09:59 +0100

    [MIG] account_invoice_production_lot: Migration to 15.0
    TT37267

--- account_invoice_production_lot/tests/test_invoice_production_lot.py
+++ account_invoice_production_lot/tests/test_invoice_production_lot.py
@@ -1,19 +1,19 @@
 # Copyright 2011 Domsense s.r.l. <http://www.domsense.com>
 # Copyright 2013 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-# Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
+# Copyright 2017 Tecnativa - Vicent Cubells
 # Copyright 2018 Alex Comba <alex.comba@agilebg.com>
 # Copyright 2020 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests import Form
-from odoo.tests.common import SavepointCase, tagged
+from odoo.tests.common import TransactionCase, tagged
 
 
 @tagged("post_install", "-at_install")
-class TestProdLot(SavepointCase):
+class TestProdLot(TransactionCase):
     @classmethod
     def setUpClass(cls):
-        super(TestProdLot, cls).setUpClass()
+        super().setUpClass()
         cls.user_company = cls.env.user.company_id
         cls.partner = cls.env["res.partner"].create(
             {"name": "Test partner", "lang": "en_US"}
@@ -21,50 +21,30 @@ class TestProdLot(SavepointCase):
         cls.product = cls.env["product.product"].create(
             {
                 "name": "Product Test",
-                "type": "product",
+                "detailed_type": "product",
                 "tracking": "lot",
                 "invoice_policy": "delivery",
+                "list_price": 15,
             }
         )
         cls.product2 = cls.env["product.product"].create(
             {
                 "name": "Product Test 2",
-                "type": "product",
+                "detailed_type": "product",
                 "tracking": "serial",
                 "invoice_policy": "delivery",
+                "list_price": 10,
             }
         )
-        cls.sale = cls.env["sale.order"].create(
-            {
-                "partner_id": cls.partner.id,
-                "partner_invoice_id": cls.partner.id,
-                "partner_shipping_id": cls.partner.id,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": cls.product.name,
-                            "product_id": cls.product.id,
-                            "product_uom_qty": 4,
-                            "product_uom": cls.product.uom_id.id,
-                            "price_unit": 15.0,
-                        },
-                    ),
-                    (
-                        0,
-                        0,
-                        {
-                            "name": cls.product2.name,
-                            "product_id": cls.product2.id,
-                            "product_uom_qty": 1,
-                            "product_uom": cls.product2.uom_id.id,
-                            "price_unit": 10.0,
-                        },
-                    ),
-                ],
-            }
-        )
+        order_form = Form(cls.env["sale.order"])
+        order_form.partner_id = cls.partner
+        with order_form.order_line.new() as line_form:
+            line_form.product_id = cls.product
+            line_form.product_uom_qty = 4
+        with order_form.order_line.new() as line_form:
+            line_form.product_id = cls.product2
+            line_form.product_uom_qty = 1
+        cls.sale = order_form.save()
         cls.lot1 = cls.env["stock.production.lot"].create(
             {
                 "name": "Lot 1",
@@ -86,39 +66,24 @@ class TestProdLot(SavepointCase):
                 "company_id": cls.user_company.id,
             }
         )
-        cls.stock_location = cls.env.ref("stock.stock_location_stock")
-        cls.customer_location = cls.env.ref("stock.stock_location_customers")
-        cls.picking_type_out = cls.env.ref("stock.picking_type_out")
 
-    def qty_on_hand(self, product, location, quantity, lot):
+    def qty_on_hand(self, product, quantity, lot):
         """Update Product quantity."""
-        wiz = self.env["stock.inventory"].create(
-            {
-                "name": "Stock Inventory",
-                "product_ids": [(4, product.id, 0)],
-                "line_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "product_id": product.id,
-                            "product_uom_id": product.uom_id.id,
-                            "product_qty": quantity,
-                            "prod_lot_id": lot.id,
-                            "location_id": location.id,
-                        },
-                    ),
-                ],
-            }
+        res = product.action_update_quantity_on_hand()
+        stock_quant_form = Form(
+            self.env["stock.quant"].with_context(**res["context"]),
+            view="stock.view_stock_quant_tree_inventory_editable",
         )
-        wiz.action_start()
-        wiz.action_validate()
+        stock_quant_form.inventory_quantity = quantity
+        stock_quant_form.lot_id = lot
+        quant = stock_quant_form.save()
+        quant.action_apply_inventory()
 
     def test_00_sale_stock_invoice_product_lot(self):
         # update quantities with their related lots
-        self.qty_on_hand(self.product, self.stock_location, 2, self.lot1)
-        self.qty_on_hand(self.product, self.stock_location, 2, self.lot2)
-        self.qty_on_hand(self.product2, self.stock_location, 1, self.serial)
+        self.qty_on_hand(self.product, 2, self.lot1)
+        self.qty_on_hand(self.product, 2, self.lot2)
+        self.qty_on_hand(self.product2, 1, self.serial)
         # confirm quotation
         self.sale.action_confirm()
         picking = self.sale.picking_ids[:1]
@@ -142,8 +107,8 @@ class TestProdLot(SavepointCase):
 
     def test_01_sale_stock_delivery_partial_invoice_product_lot(self):
         # update quantities with their related lots
-        self.qty_on_hand(self.product, self.stock_location, 3, self.lot1)
-        self.qty_on_hand(self.product, self.stock_location, 3, self.lot2)
+        self.qty_on_hand(self.product, 3, self.lot1)
+        self.qty_on_hand(self.product, 3, self.lot2)
         # confirm quotation
         self.sale.action_confirm()
         picking = self.sale.picking_ids[:1]
@@ -154,7 +119,7 @@ class TestProdLot(SavepointCase):
         backorder_wizard_dict = picking.button_validate()
         backorder_wiz = Form(
             self.env[backorder_wizard_dict["res_model"]].with_context(
-                backorder_wizard_dict["context"]
+                **backorder_wizard_dict["context"]
             )
         ).save()
         backorder_wiz.process()
@@ -172,8 +137,8 @@ class TestProdLot(SavepointCase):
 
     def test_02_sale_stock_delivery_partial_invoice_product_lot(self):
         # update quantities with their related lots
-        self.qty_on_hand(self.product, self.stock_location, 3, self.lot1)
-        self.qty_on_hand(self.product, self.stock_location, 3, self.lot2)
+        self.qty_on_hand(self.product, 3, self.lot1)
+        self.qty_on_hand(self.product, 3, self.lot2)
         # confirm quotation
         self.sale.action_confirm()
         picking = self.sale.picking_ids[:1]
@@ -185,7 +150,7 @@ class TestProdLot(SavepointCase):
         backorder_wizard_dict = picking.button_validate()
         backorder_wiz = Form(
             self.env[backorder_wizard_dict["res_model"]].with_context(
-                backorder_wizard_dict["context"]
+                **backorder_wizard_dict["context"]
             )
         ).save()
         backorder_wiz.process()
