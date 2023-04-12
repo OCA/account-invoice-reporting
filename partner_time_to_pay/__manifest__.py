6216a5290bb284cf487c5cf19a4ff58df9b17927
Eduardo De Miguel <edu@moduon.team>
2023-04-12 08:30:32 +0200

    [IMP] partner_time_to_pay: AVG Days for Last Year

--- partner_time_to_pay/__manifest__.py
+++ partner_time_to_pay/__manifest__.py
@@ -4,13 +4,13 @@
 {
     "name": "Partner Time to Pay",
     "summary": "Add receivables and payables statistics to partners",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     "license": "AGPL-3",
     "development_status": "Beta",
     "author": "Open Source Integrators, Odoo Community Association (OCA), Moduon",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoice-reporting",
-    "maintainers": ["max3903", "rafaelbn"],
+    "maintainers": ["max3903", "rafaelbn", "Shide"],
     "depends": ["account"],
     "data": [
         "views/account_move_view.xml",
