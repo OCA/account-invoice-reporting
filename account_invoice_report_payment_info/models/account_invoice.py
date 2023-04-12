f56023f1133bf0cb15e9066ebd788f6a7261be16
Stefan <stefan.ungureanu@tecnativa.com>
2023-04-12 16:34:01 +0200

    [MIG] account_invoice_report_payment_info: Migration to 16.0

--- account_invoice_report_payment_info/models/account_invoice.py
+++ account_invoice_report_payment_info/models/account_invoice.py
@@ -7,18 +7,18 @@ from odoo import models
 class AccountInvoice(models.Model):
     _inherit = "account.move"
 
-    def _get_reconciled_info_JSON_values(self):
-        res = super()._get_reconciled_info_JSON_values()
-        if not res:
+    def _compute_payments_widget_reconciled_info(self):
+        res = super()._compute_payments_widget_reconciled_info()
+        if not res and not self.invoice_payments_widget:
             return res
         info_pattern = (
             self.env["ir.config_parameter"]
             .sudo()
             .get_param("account_invoice_report_payment_info.info_pattern", default="")
         )
-        Payment = self.env["account.move.line"]
-        for payment_dict in res:
-            payment = Payment.browse(payment_dict["payment_id"])
-            payment_dict["move_ref"] = payment.move_id.ref
+        Move = self.env["account.move"]
+        for payment_dict in self.invoice_payments_widget["content"]:
+            move = Move.browse(payment_dict["move_id"])
+            payment_dict["move_ref"] = move.ref
             payment_dict["extra_info"] = info_pattern.format(**payment_dict)
         return res
