6d7775f30dbf8da2152de767ce118fea99209bb5
heliaktiv <heli.aktiv@gmail.com>
2023-08-25 14:57:16 +0530

    [MIG] account_invoice_production_lot: Migration to 16.0

--- account_invoice_production_lot/__manifest__.py
+++ account_invoice_production_lot/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Invoice Production Lots",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Agile Business Group," "Tecnativa," "Odoo Community Association (OCA)",
     "summary": "Display delivered serial numbers in invoice",
     "website": "https://github.com/OCA/account-invoice-reporting",
