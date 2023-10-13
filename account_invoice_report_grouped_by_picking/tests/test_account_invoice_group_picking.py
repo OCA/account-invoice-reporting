70c7019560635b92cb496ad4ea281e83a944b0e7
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-13 14:10:21 +0200

    [IMP] account_invoice_report_grouped_by_picking: test performance improvement - Include context keys for avoiding mail operations overhead.

--- account_invoice_report_grouped_by_picking/tests/test_account_invoice_group_picking.py
+++ account_invoice_report_grouped_by_picking/tests/test_account_invoice_group_picking.py
@@ -13,6 +13,16 @@ class TestAccountInvoiceGroupPicking(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestAccountInvoiceGroupPicking, cls).setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
         cls.product = cls.env["product.product"].create(
             {
                 "name": "Product for test",
