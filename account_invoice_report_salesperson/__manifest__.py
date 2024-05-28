25b6b5daf0ce439a1d6392f3d1a0dad648abecb2
Eduardo De Miguel <edu@moduon.team>
2024-05-28 13:04:36 +0200

    [ADD] account_invoice_report_salesperson

---
+++ account_invoice_report_salesperson/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Moduon Team S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+
+{
+    "name": "Account Invoice Report Salesperson",
+    "summary": "Salesperson info in Invoice report",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Account",
+    "website": "https://github.com/OCA/account-invoice-reporting",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["Shide"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+}
