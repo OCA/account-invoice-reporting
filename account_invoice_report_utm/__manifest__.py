7234ca690b1bbcf55a6797cfd89874a6225b1dfd
Olivier Nibart <olivier.nibart@akretion.com>
2024-10-21 12:17:52 +0200

    [MIG] account_invoice_report_utm: Migration to 15.0

--- account_invoice_report_utm/__manifest__.py
+++ account_invoice_report_utm/__manifest__.py
@@ -2,11 +2,11 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "Account Invoice Report UTM Campaign",
+    "name": "Account Invoice Report UTM",
     "summary": """
     Add the Campaign, Medium, and Source fields to the Invoice Analysis report.
     """,
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoice-reporting",
     "author": "Akretion, Odoo Community Association (OCA)",
