433ffd02c73233f6e52accaf449f5d7b130bacc0
Víctor Martínez <victor.martinez@tecnativa.com>
2025-06-24 13:45:12 +0200

    [MIG] account_reporting_volume: Migration to 18.0
    TT54409

--- account_reporting_volume/models/account_invoice_report.py
+++ account_reporting_volume/models/account_invoice_report.py
@@ -1,7 +1,9 @@
 # Copyright 2022 Tecnativa - Carlos Roca
+# Copyright 2025 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.tools import SQL
 
 
 class AccountInvoiceReport(models.Model):
@@ -9,16 +11,17 @@ class AccountInvoiceReport(models.Model):
 
     volume = fields.Float(digits="Volume")
 
-    def _select(self):
-        select_str = super()._select()
-        select_str += """
-            , COALESCE(
-                (product.volume * (
-                    CASE
-                    WHEN move.move_type IN ('in_invoice','out_refund','in_receipt') THEN -1
-                    ELSE 1 END
-                ) * line.quantity
-                / uom_line.factor * uom_template.factor
-            ), 0.0) as volume
+    def _select(self) -> SQL:
+        return SQL(
             """
-        return select_str
+                %s,
+                COALESCE(
+                    (product.volume *
+                    (CASE WHEN move.move_type IN
+                    ('in_invoice','out_refund','in_receipt') THEN -1 ELSE 1 END)
+                    * line.quantity / uom_line.factor * uom_template.factor),
+                    0.0
+                ) as volume
+            """,
+            super()._select(),
+        )
