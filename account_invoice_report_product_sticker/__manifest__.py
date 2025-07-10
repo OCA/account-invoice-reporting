a5563dccc904af0774dcb11494d0e578d70ac624
Andrii Kompaniiets <andrii@moduon.team>
2025-07-10 11:34:12 +0200

    [MIG] account_invoice_report_product_sticker: Migration to 18.0

--- account_invoice_report_product_sticker/__manifest__.py
+++ account_invoice_report_product_sticker/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Invoice Report - Product Sticker",
     "summary": "Display Product Stickers on Invoice Reports",
-    "version": "16.0.1.0.4",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Accounting/Accounting",
     "website": "https://github.com/OCA/account-invoice-reporting",
