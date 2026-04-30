e179f95cb301f5eab7668f35f69af2188bc9dec8
Bhavesh Heliconia <oca@heliconia.in>
2026-04-30 17:56:46 +0530

    [IMP] account_invoice_line_report: pre-commit auto fixes

--- account_invoice_line_report/__manifest__.py
+++ account_invoice_line_report/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "18.0.1.0.1",
     "category": "Account",
     "website": "https://github.com/OCA/account-invoice-reporting",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["account"],
     "data": ["report/account_invoice_report_view.xml"],
