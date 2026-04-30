9d2bce33e3aebf6c8479a71d8a1bd27a1c9d0389
Bhavesh Heliconia <oca@heliconia.in>
2026-04-30 18:01:15 +0530

    [MIG] account_invoice_line_report: Migration to 19.0

--- account_invoice_line_report/__manifest__.py
+++ account_invoice_line_report/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Account Invoice Line Report",
     "summary": "New view to manage invoice lines information",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/account-invoice-reporting",
     "author": "Tecnativa, Odoo Community Association (OCA)",
