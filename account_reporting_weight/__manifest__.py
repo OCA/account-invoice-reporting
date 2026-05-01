350d84a6ff1aa8edf7c0af0f0c60ea46f4b66504
Bhavesh Heliconia <oca@heliconia.in>
2026-05-01 14:04:15 +0530

    [IMP] account_reporting_weight: pre-commit auto fixes

--- account_reporting_weight/__manifest__.py
+++ account_reporting_weight/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Weights in the invoices analysis view",
     "version": "18.0.1.0.0",
-    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "Tecnativa,Odoo Community Association (OCA)",
     "category": "Inventory, Logistics, Warehousing",
     "development_status": "Production/Stable",
     "license": "AGPL-3",
