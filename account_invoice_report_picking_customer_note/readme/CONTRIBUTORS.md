1abbc4a16c0c72e330e36f77e4ed60df7ab229ba
Bhavesh Heliconia <oca@heliconia.in>
2026-05-04 12:34:17 +0530

    [MIG] account_invoice_report_picking_customer_note: Migration to 19.0

--- account_invoice_report_picking_customer_note/readme/CONTRIBUTORS.md
+++ account_invoice_report_picking_customer_note/readme/CONTRIBUTORS.md
@@ -2,3 +2,5 @@
   - Rafael Blasco
   - David Vidal
   - Andrii Kompaniiets
+- [Heliconia Solutions Pvt. Ltd.](https://www.heliconia.io)
+  - Bhavesh Heliconia
--- account_invoice_report_picking_customer_note/tests/test_print_picking_customer_note.py
+++ account_invoice_report_picking_customer_note/tests/test_print_picking_customer_note.py
@@ -1,7 +1,7 @@
 # Copyright 2025 Moduon Team S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields
+from odoo import Command, fields
 from odoo.tests import Form, TransactionCase
 
 
@@ -9,24 +9,87 @@ class TestSaleStockPickingNote(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.journal = cls.env["account.journal"].search(
+            [("type", "=", "sale"), ("company_id", "=", cls.env.company.id)], limit=1
+        )
+        if not cls.journal:
+            cls.journal = cls.env["account.journal"].create(
+                {
+                    "name": "Test Sales Journal",
+                    "code": "TSJ",
+                    "type": "sale",
+                    "company_id": cls.env.company.id,
+                }
+            )
+        cls.account = cls.env["account.account"].search(
+            [
+                ("account_type", "=", "income"),
+                ("company_ids", "in", cls.env.company.ids),
+            ],
+            limit=1,
+        )
+        if not cls.account:
+            cls.account = cls.env["account.account"].create(
+                {
+                    "name": "Test Revenue Account",
+                    "code": "REV101",
+                    "account_type": "income",
+                    "company_ids": [Command.set(cls.env.company.ids)],
+                }
+            )
+        cls.account_receivable = cls.env["account.account"].search(
+            [
+                ("account_type", "=", "asset_receivable"),
+                ("company_ids", "in", cls.env.company.ids),
+            ],
+            limit=1,
+        )
+        if not cls.account_receivable:
+            cls.account_receivable = cls.env["account.account"].create(
+                {
+                    "name": "Test Receivable Account",
+                    "code": "REC101",
+                    "account_type": "asset_receivable",
+                    "company_ids": [Command.set(cls.env.company.ids)],
+                    "reconcile": True,
+                }
+            )
+        cls.account_payable = cls.env["account.account"].search(
+            [
+                ("account_type", "=", "liability_payable"),
+                ("company_ids", "in", cls.env.company.ids),
+            ],
+            limit=1,
+        )
+        if not cls.account_payable:
+            cls.account_payable = cls.env["account.account"].create(
+                {
+                    "name": "Test Payable Account",
+                    "code": "PAY101",
+                    "account_type": "liability_payable",
+                    "company_ids": [Command.set(cls.env.company.ids)],
+                    "reconcile": True,
+                }
+            )
         cls.partner = cls.env["res.partner"].create(
             {
                 "name": "Mr. Odoo",
                 "picking_customer_note": "Test customer note",
+                "property_account_receivable_id": cls.account_receivable.id,
+                "property_account_payable_id": cls.account_payable.id,
             }
         )
 
     def _create_invoice(self):
         invoice_vals = [
-            (
-                0,
-                0,
+            Command.create(
                 {
                     "product_id": False,
                     "quantity": 1.0,
                     "name": "Line 1",
                     "price_unit": 200.00,
-                },
+                    "account_id": self.account.id,
+                }
             )
         ]
         invoice = (
@@ -34,9 +97,7 @@ class TestSaleStockPickingNote(TransactionCase):
             .with_context(check_move_validity=False)
             .create(
                 {
-                    "journal_id": self.env["account.journal"]
-                    .search([("type", "=", "sale")], limit=1)
-                    .id,
+                    "journal_id": self.journal.id,
                     "partner_id": self.partner.id,
                     "move_type": "out_invoice",
                     "invoice_line_ids": invoice_vals,
@@ -52,24 +113,23 @@ class TestSaleStockPickingNote(TransactionCase):
                 "type": "consu",
                 "standard_price": 1.0,
                 "list_price": 1.0,
+                "property_account_income_id": self.account.id,
             }
         )
         so_vals = {
             "partner_id": self.partner.id,
             "date_order": fields.Datetime.now(),
             "order_line": [
-                (
-                    0,
-                    0,
+                Command.create(
                     {
                         "name": product.name,
                         "product_id": product.id,
                         "price_unit": product.list_price,
-                    },
+                    }
                 )
             ],
         }
-        return self.env["sale.order"].create(so_vals)
+        return self.env["sale.order"].sudo().create(so_vals)
 
     def test_print_picking_customer_note_in_invoice(self):
         invoice_form = Form(self._create_invoice())
@@ -91,24 +151,23 @@ class TestSaleStockPickingNote(TransactionCase):
                 "type": "consu",
                 "standard_price": 1.0,
                 "list_price": 1.0,
+                "property_account_income_id": self.account.id,
             }
         )
         so_vals = {
             "partner_id": self.partner.id,
             "date_order": fields.Datetime.now(),
             "order_line": [
-                (
-                    0,
-                    0,
+                Command.create(
                     {
                         "name": product.name,
                         "product_id": product.id,
                         "price_unit": product.list_price,
-                    },
+                    }
                 )
             ],
         }
-        so = self.env["sale.order"].create(so_vals)
+        so = self.env["sale.order"].sudo().create(so_vals)
         so.picking_customer_note = picking_customer_note
         so.action_confirm()
         invoice = so._create_invoices()
@@ -121,18 +180,22 @@ class TestSaleStockPickingNote(TransactionCase):
         )
         self.assertRegex(res, picking_customer_note)
 
-        def test_print_picking_customer_note_in_invoice_more_one_sale_orders(self):
-            invoice = self._create_invoice()
-            so_1 = self._create_sale_order()
-            so_2 = self._create_sale_order()
-            so_1.picking_customer_note = "Picking Note"
-            so_2.picking_customer_note = "Picking Note SO 2"
-            invoice.invoice_line_ids.sale_line_ids.order_id = [so_1, so_2]
-            invoice.action_post()
-            report = self.env.ref("account.account_invoices_without_payment")
-            res = str(
-                report._render_qweb_html(
-                    "account.account_invoices_without_payment", invoice.id
-                )[0]
-            )
-            self.assertRegex(res, self.partner.picking_customer_note)
+    def test_print_picking_customer_note_in_invoice_more_one_sale_orders(self):
+        invoice = self._create_invoice()
+        so_1 = self._create_sale_order()
+        so_2 = self._create_sale_order()
+        so_1.picking_customer_note = "Picking Note"
+        so_2.picking_customer_note = "Picking Note SO 2"
+
+        invoice.invoice_line_ids.write(
+            {"sale_line_ids": [Command.set((so_1.order_line | so_2.order_line).ids)]}
+        )
+
+        invoice.action_post()
+        report = self.env.ref("account.account_invoices_without_payment")
+        res = str(
+            report._render_qweb_html(
+                "account.account_invoices_without_payment", invoice.id
+            )[0]
+        )
+        self.assertRegex(res, self.partner.picking_customer_note)
